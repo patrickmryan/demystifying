b9c34735db1ab5d4bec09017f33db1f0ee4ec0a9
Patrick M. Ryan <pmryan@us.ibm.com>
2019-05-13 13:40:40 -0400

    refactoring into Post class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,46 +12,57 @@ class ApplicationController < ActionController::Base
   end
 
   def list_posts
-    connection = self.get_connection
-
-    results = connection.execute('select * from posts')
-    render 'application/list_posts', locals: { posts: results }
+    # connection = self.get_connection
+    # results = connection.execute('select * from posts')
+    posts = Post.all
+    render 'application/list_posts', locals: { posts: posts }
   end
 
   def show_post
-    connection = self.get_connection
-    result = find_post_by_id(params['id'])
-    render 'application/show_post', locals: { post: result }
+    # connection = self.get_connection
+    # result = find_post_by_id(params['id'])
+    post = Post.find(params['id'])
+    render 'application/show_post', locals: { post: post }
+
   end
 
   def create_post
-    connection = self.get_connection
+    # connection = self.get_connection
+    #
+    # insert_query = <<-SQL
+    #   INSERT INTO posts (title, body, author, created_at)
+    #   VALUES (?, ?, ?, ?)
+    # SQL
+    #
+    # connection.execute(insert_query,
+    #   params['title'],
+    #   params['body'],
+    #   params['author'],
+    #   Date.current.to_s)
 
-    insert_query = <<-SQL
-      INSERT INTO posts (title, body, author, created_at)
-      VALUES (?, ?, ?, ?)
-    SQL
-
-    connection.execute(insert_query,
-      params['title'],
-      params['body'],
-      params['author'],
-      Date.current.to_s)
+    post = Post.new('title' => params['title'],
+                    'body' => params['body'],
+                    'author' => params['author'])
+    post.save
 
     redirect_to '/list_posts'
 
   end
 
   def delete_post
-    self.get_connection.execute('delete from posts where posts.id = ?', params['id'])
+#    self.get_connection.execute('delete from posts where posts.id = ?', params['id'])
+    post = Post.find(params['id'])
+    post.destroy
     redirect_to '/list_posts'
 
   end
 
   def edit_post
-    connection = self.get_connection
-    result = find_post_by_id(params['id'])
-    render 'application/edit_post', locals: { post: result }
+    # connection = self.get_connection
+    # result = find_post_by_id(params['id'])
+
+    post = Post.find(params['id'])
+    render 'application/edit_post', locals: { post: post }
 
   end
 
@@ -71,9 +82,9 @@ class ApplicationController < ActionController::Base
     redirect_to '/list_posts'
   end
 
-  def find_post_by_id(id)
-    self.get_connection.execute("SELECT * FROM posts WHERE posts.id = ? LIMIT 1", id).first
-  end
+  # def find_post_by_id(id)
+  #   self.get_connection.execute("SELECT * FROM posts WHERE posts.id = ? LIMIT 1", id).first
+  # end
 
 
   def get_connection
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,32 +2,84 @@ class Post
   attr_reader :id, :title, :body, :author, :created_at
 
   def initialize(attributes={})
-    @id = attributes['id']
+    set_attributes(attributes)
+  end
+
+  def set_attributes(attributes)
+    @id = attributes['id'] if new_record?
     @title = attributes['title']
     @body = attributes['body']
     @author = attributes['author']
-    @created_at = attributes['created_at']
+    @created_at ||= attributes['created_at']
+  end
+
+  def new_record?
+    id.nil?
   end
 
   def save
+    if new_record?
+      self.insert
+    else
+      self.update
+    end
+  end
+
+  def insert
     insert_query = <<-SQL
       INSERT INTO posts (title, body, author, created_at)
       VALUES (?, ?, ?, ?)
     SQL
 
-    self.connection.execute insert_query,
+    connection.execute insert_query,
       title,
       body,
       author,
       Date.current.to_s
   end
 
+  def update
+    update_query = <<-SQL
+      UPDATE posts
+      SET title      = ?,
+          body       = ?,
+          author     = ?
+      WHERE posts.id = ?
+    SQL
+
+    connection.execute update_query,
+      title,
+      body,
+      author,
+      id
+  end
+
+  def destroy
+    self.connection.execute("delete from posts where posts.id = ?",id)
+  end
+
   # ...
 
   def connection
-    db_connection = SQLite3::Database.new('db/development.sqlite3')
+    self.class.connection
+  end
+
+  def self.connection
+    db_connection = SQLite3::Database.new 'db/development.sqlite3'
     db_connection.results_as_hash = true
     db_connection
   end
+
+  def self.find(id)
+    post_hash = connection.execute("SELECT * FROM posts WHERE posts.id = ? LIMIT 1", id).first
+    Post.new(post_hash)
+  end
+
+  def self.all
+    post_hashes = connection.execute("SELECT * FROM posts")
+    post_hashes.map do |post_hash|
+      Post.new(post_hash)
+    end
+  end  
 
 end
