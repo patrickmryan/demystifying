23849f3280525200ccb29fbf73b7ac90c5988d6c
Patrick M. Ryan <pmryan@us.ibm.com>
2019-05-13 11:07:33 -0400

    can now edit post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
     connection = self.get_connection
 
     results = connection.execute('select * from posts')
-    render 'application/lists_posts', locals: { posts: results }
+    render 'application/list_posts', locals: { posts: results }
   end
 
   def show_post
@@ -24,6 +24,49 @@ class ApplicationController < ActionController::Base
       params['id'])
     render 'application/show_post', locals: { post: results.first }
   end
+
+  def create_post
+    connection = self.get_connection
+
+    insert_query = <<-SQL
+      INSERT INTO posts (title, body, author, created_at)
+      VALUES (?, ?, ?, ?)
+    SQL
+
+    connection.execute(insert_query,
+      params['title'],
+      params['body'],
+      params['author'],
+      Date.current.to_s)
+
+    redirect_to '/list_posts'
+
+  end
+
+  def edit_post
+    connection = self.get_connection
+    result = connection.execute("SELECT * FROM posts WHERE posts.id = ? LIMIT 1", params['id']).first
+
+    render 'application/edit_post', locals: { post: result }
+
+  end
+
+
+  def update_post
+    connection = self.get_connection
+
+    update_query = <<-SQL
+      UPDATE posts
+      SET title      = ?,
+          body       = ?,
+          author     = ?
+      WHERE posts.id = ?
+    SQL
+    connection.execute update_query, params['title'], params['body'], params['author'], params['id']
+
+    redirect_to '/list_posts'
+  end
+
 
   def get_connection
     connection = SQLite3::Database.new 'db/development.sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   get '/list_posts' => 'application#list_posts'
   get '/show_post/:id' => 'application#show_post'
+  get '/new_post' => 'application#new_post'
+  post '/create_post' => 'application#create_post'
+  get '/edit_post/:id' => 'application#edit_post'
+  post '/update_post/:id' => 'application#update_post'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
