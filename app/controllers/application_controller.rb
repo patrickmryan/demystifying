1f6b274e96948cd96a71aa5a774ae55b2ad8a439
Patrick M. Ryan <pmryan@us.ibm.com>
2019-05-13 10:24:15 -0400

    added post links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,34 @@
+#require 'sqlite3'
+
 class ApplicationController < ActionController::Base
+  def hello_world
+    #self.render(plain: 'Hello World')
+    #self.render(inline: '<em>Hello, World</em>')
+    #self.render('application/hello_world')
+
+    aName = params['name'] || 'Default'
+    self.render('application/hello_world', locals: {name: aName})
+
+  end
+
+  def list_posts
+    connection = self.get_connection
+
+    results = connection.execute('select * from posts')
+    render 'application/lists_posts', locals: { posts: results }
+  end
+
+  def show_post
+    connection = self.get_connection
+    results = connection.execute('select * from posts where posts.id = ? limit 1',
+      params['id'])
+    render 'application/show_post', locals: { post: results.first }
+  end
+
+  def get_connection
+    connection = SQLite3::Database.new 'db/development.sqlite3'
+    connection.results_as_hash = true
+    connection
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get({'/hello_world' => 'application#hello_world'})
+  get('/hello/:name' => 'application#hello_world')
+
+  get '/list_posts' => 'application#list_posts'
+  get '/show_post/:id' => 'application#show_post'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
